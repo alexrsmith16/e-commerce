e2f9cd340974f01987583dcb3d2ce4353a5625a9
Alex Smith <alexrsmith16@gmail.com>
2020-01-06 07:48:34 -0700

    finished main navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useRouteMatch
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Products from './components/products';
+import Cart from './components/cart';
+import Profile from './components/profile';
+import Details from './components/details';
+
+function CustomLink(props) {
+  let match = useRouteMatch({
+    path: props.to,
+    exact: true
+  });
+  console.log(match);
+  let styles = 'item';
+  if (match) styles += ' active';
+  return (<Link className={styles} to={props.to} >{props.children}</Link>);
+}
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <Router>
+          <div className='app'>
+            <div className='ui four item menu'>
+              <Link to='./' className='header item'>Store</Link>
+              {/* <div className='center menu'> */}
+                <CustomLink to='/products' >Products</CustomLink>
+                <CustomLink to='/profile' >Profile</CustomLink>
+                <CustomLink to='/cart' >Cart</CustomLink>
+              {/* </div> */}
+            </div>
+            <Switch>
+              <Route exact path='/' />
+              <Route exact path='/products' component={Products} />
+              <Route exact path='/cart' component={Cart} />
+              <Route exact path='/profile' component={Profile} />
+              <Route path='/products/:productId' component={Details} />
+              <Route render={() => (<div>404</div>)} />
+            </Switch>
+            <div className='ui footer'>Footer</div>
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/products/product.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Product extends React.Component {
+
+	render() {
+		return (<div>Item</div>)
+	}
+}
+
+export default Product;
