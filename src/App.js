a84ac6f8db7ca3fbc470e06d8a04d5c0280316a0
Alex Smith <alexrsmith16@gmail.com>
2020-01-27 19:54:47 -0700

    commiting some progress

--- src/App.js
+++ src/App.js
@@ -11,21 +11,48 @@ import Products from './components/products';
 import Cart from './components/cart';
 import Profile from './components/profile';
 import Details from './components/details';
+import { createStore } from 'redux';
+
+
+function 	get(url) {
+  return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = () => resolve(JSON.parse(xhr.responseText));
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+  });
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      break;
+    case "REMOVE":
+      break;
+  }
+  return state;
+}
+
+const store = createStore(reducer, {products: [], cart: []});
 
 function CustomLink(props) {
   let match = useRouteMatch({
     path: props.to,
     exact: true
   });
-  console.log(match);
   let styles = 'item';
   if (match) styles += ' active';
   return (<Link className={styles} to={props.to} >{props.children}</Link>);
 }
 
 class App extends React.Component {
-  
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
+  }
+
   render() {
+    console.log(store.getState());
     return (
       <div>
         <Router>
@@ -40,9 +67,9 @@ class App extends React.Component {
             </div>
             <Switch>
               <Route exact path='/' />
-              <Route exact path='/products' component={Products} />
-              <Route exact path='/cart' component={Cart} />
-              <Route exact path='/profile' component={Profile} />
+              <Route exact path='/products'><Products store={store} /></Route>
+              <Route exact path='/cart'><Cart store={store} /></Route>
+              <Route exact path='/profile'><Profile store={store} /></Route>
               <Route path='/products/:productId' component={Details} />
               <Route render={() => (<div>404</div>)} />
             </Switch>
--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class Products extends React.Component {
+	constructor(props) {
+		super(props);
+		console.log(props.store.getState());
+	}
 
 	render() {
 		return (<div>Products</div>)
